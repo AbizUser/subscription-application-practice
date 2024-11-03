97b569a1256f177134463c653d40d18850b4d692
AbizUser <aman.biz.t23@gmail.com>
2024-11-04 08:14:54 +0900

    add subscription status update process

--- app/api/stripe-hooks/route.ts
+++ app/api/stripe-hooks/route.ts
@@ -23,23 +23,49 @@ export async function POST(req: NextRequest) {
     switch (event.type) {
       case "customer.subscription.created":
         const customerSubscriptionCreated = event.data.object;
-        await supabase.from("profile").update({
+        await supabase
+          .from("profile")
+          .update({
           is_subscribed: true,
           interval: customerSubscriptionCreated.items.data[0].plan.interval
-        }).eq("stripe_customer", event.data.object.customer);
-
-        break;
-      case "customer.subscription.deleted":
-      const customerSubscriptionDeleted = event.data.object;
+          })
+          .eq("stripe_customer", event.data.object.customer);
         break;
       case "customer.subscription.updated":
-      const customerSubscriptionupdated = event.data.object;
+        const customerSubscriptionUpdated = event.data.object;
+
+        if ( customerSubscriptionUpdated.status === "canceled") {
+          await supabase
+          .from("profile")
+          .update({
+          is_subscribed: false,
+          interval: null,
+          })
+          .eq("stripe_customer", event.data.object.customer);
+          break;
+        }else {
+          await supabase
+          .from("profile")
+          .update({
+          is_subscribed: true,
+          interval: customerSubscriptionUpdated.items.data[0].plan.interval
+          })
+          .eq("stripe_customer", event.data.object.customer);
         break;
-    
+        }
+      case "customer.subscription.deleted":
+        await supabase
+        .from("profile")
+        .update({
+        is_subscribed: false,
+        interval: null,
+        })
+        .eq("stripe_customer", event.data.object.customer);
+        break;
       default:
         break;
     }
-    console.log(event)
+
     return NextResponse.json({recieved: true });
   } catch (err: any) {
     return NextResponse.json(`webhook Error: ${err.message}`, { status: 401 });
